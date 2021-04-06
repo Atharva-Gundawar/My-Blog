a28a2b02a9f31a3d97584ab2d8efb7e35e2bcbda
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-04-07 02:55:18 +0530

    feat: Using ListView

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic import ListView
 
 @login_required
 def home(request):
@@ -11,6 +11,13 @@ def home(request):
         'timep' : timezone.now()
     }
     return render(request, 'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
 
 
 def about(request):
