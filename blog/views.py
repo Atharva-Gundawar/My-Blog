bfd091b484984212ea12baf9c656add2bc96135b
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-04-07 13:08:00 +0530

    feat: Posts updating

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import (ListView,
                                     DetailView,
-                                    CreateView
-                                    # DeleteView
+                                    CreateView,
+                                    UpdateView
 )
+from django.contrib.auth.models import User
+
 
 @login_required
 def home(request):
@@ -33,6 +35,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return (post.author == self.request.user)# or (self.request.user == User.objects.get(username="atharva"))
     
     
 
